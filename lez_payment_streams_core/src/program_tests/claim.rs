b6b362cd25a41dcc1f665e384c0a67a71565e242
Sergei Tikhomirov <sergey.s.tikhomirov@gmail.com>
2026-04-20 23:11:50 +0200

    add unauthorized tests

--- lez_payment_streams_core/src/program_tests/claim.rs
+++ lez_payment_streams_core/src/program_tests/claim.rs
@@ -8,7 +8,7 @@ use nssa_core::{
 use crate::{
     test_helpers::{create_keypair, derive_stream_pda, force_mock_timestamp_account},
     MockTimestamp, StreamConfig, StreamId, StreamState, Timestamp, TokensPerSecond, VaultConfig,
-    ERR_ZERO_CLAIM_AMOUNT,
+    ERR_CLAIM_UNAUTHORIZED, ERR_ZERO_CLAIM_AMOUNT,
 };
 
 use super::common::{
@@ -17,7 +17,7 @@ use super::common::{
     ClaimStreamIxAccounts, CloseStreamIxAccounts, DEFAULT_OWNER_GENESIS_BALANCE,
     DEFAULT_STREAM_TEST_DEPOSIT,
 };
-use crate::harness_seeds::{SEED_MOCK_CLOCK, SEED_PROVIDER};
+use crate::harness_seeds::{SEED_ALT_SIGNER, SEED_MOCK_CLOCK, SEED_PROVIDER};
 
 #[test]
 fn test_claim_transfers_balance() {
@@ -134,6 +134,100 @@ fn test_claim_transfers_balance() {
         provider_balance_after,
         provider_balance_before.saturating_add(payout)
     );
+}
+
+#[test]
+fn test_claim_unauthorized_fails() {
+    let deposit_amount = DEFAULT_STREAM_TEST_DEPOSIT;
+    let allocation = 200 as Balance;
+    let rate = 10 as TokensPerSecond;
+    let t0: Timestamp = 12_345;
+    let t1: Timestamp = t0 + 5;
+
+    let (_, mock_clock_account_id) = create_keypair(SEED_MOCK_CLOCK);
+    let (_, provider_account_id) = create_keypair(SEED_PROVIDER);
+    let (alt_signer_private_key, alt_signer_account_id) = create_keypair(SEED_ALT_SIGNER);
+
+    let (
+        mut state,
+        program_id,
+        owner_private_key,
+        owner_account_id,
+        vault_id,
+        vault_config_account_id,
+        vault_holding_account_id,
+    ) = state_deposited_with_mock_clock_and_provider(
+        DEFAULT_OWNER_GENESIS_BALANCE,
+        deposit_amount,
+        mock_clock_account_id,
+        t0,
+        provider_account_id,
+    );
+
+    let stream_id = StreamId::MIN;
+    let stream_pda = derive_stream_pda(program_id, vault_config_account_id, stream_id);
+
+    let stream_accounts_owner = [
+        vault_config_account_id,
+        vault_holding_account_id,
+        stream_pda,
+        owner_account_id,
+        mock_clock_account_id,
+    ];
+    transition_ok(
+        &mut state,
+        &signed_create_stream(
+            program_id,
+            vault_id,
+            stream_id,
+            provider_account_id,
+            rate,
+            allocation,
+            &stream_accounts_owner,
+            Nonce(2),
+            &owner_private_key,
+        ),
+        3 as BlockId,
+        "create_stream failed",
+    );
+
+    force_mock_timestamp_account(&mut state, mock_clock_account_id, MockTimestamp::new(t1));
+
+    transition_ok(
+        &mut state,
+        &signed_sync_stream(
+            program_id,
+            vault_id,
+            stream_id,
+            &stream_accounts_owner,
+            Nonce(3),
+            &owner_private_key,
+        ),
+        4 as BlockId,
+        "sync_stream failed",
+    );
+
+    let claim_accounts: ClaimStreamIxAccounts = [
+        vault_config_account_id,
+        vault_holding_account_id,
+        stream_pda,
+        owner_account_id,
+        alt_signer_account_id,
+        mock_clock_account_id,
+    ];
+
+    let r = state.transition_from_public_transaction(
+        &signed_claim_stream(
+            program_id,
+            vault_id,
+            stream_id,
+            &claim_accounts,
+            Nonce(0),
+            &alt_signer_private_key,
+        ),
+        5 as BlockId,
+    );
+    assert_execution_failed_with_code(r, ERR_CLAIM_UNAUTHORIZED);
 }
 
 #[test]
--- lez_payment_streams_core/src/program_tests/close_stream.rs
+++ lez_payment_streams_core/src/program_tests/close_stream.rs
@@ -8,7 +8,7 @@ use nssa_core::{
 use crate::{
     test_helpers::{create_keypair, derive_stream_pda, force_mock_timestamp_account},
     MockTimestamp, StreamConfig, StreamId, StreamState, Timestamp, TokensPerSecond, VaultConfig,
-    ERR_STREAM_CLOSED,
+    ERR_CLOSE_UNAUTHORIZED, ERR_STREAM_CLOSED,
 };
 
 use super::common::{
@@ -16,7 +16,7 @@ use super::common::{
     state_deposited_with_mock_clock, transition_ok, CloseStreamIxAccounts, force_stream_state_closed,
     DEFAULT_MOCK_CLOCK_INITIAL_TS, DEFAULT_OWNER_GENESIS_BALANCE, DEFAULT_STREAM_TEST_DEPOSIT,
 };
-use crate::harness_seeds::{SEED_MOCK_CLOCK, SEED_PROVIDER};
+use crate::harness_seeds::{SEED_ALT_SIGNER, SEED_MOCK_CLOCK, SEED_PROVIDER};
 
 #[test]
 fn test_close_returns_unaccrued() {
@@ -130,6 +130,100 @@ fn test_close_returns_unaccrued() {
     let holding_balance = state.get_account_by_id(vault_holding_account_id).balance;
     let unallocated = holding_balance.saturating_sub(vault_after.total_allocated);
     assert_eq!(unallocated, 450 as Balance);
+}
+
+#[test]
+fn test_close_stream_unauthorized_fails() {
+    let owner_balance_start = DEFAULT_OWNER_GENESIS_BALANCE;
+    let deposit_amount = DEFAULT_STREAM_TEST_DEPOSIT;
+    let allocation = 200 as Balance;
+    let rate = 10 as TokensPerSecond;
+    let t0: Timestamp = 12_345;
+    let t1: Timestamp = t0 + 5;
+
+    let (_, mock_clock_account_id) = create_keypair(SEED_MOCK_CLOCK);
+    let (_, provider_account_id) = create_keypair(SEED_PROVIDER);
+    let (alt_signer_private_key, alt_signer_account_id) = create_keypair(SEED_ALT_SIGNER);
+
+    let (
+        mut state,
+        program_id,
+        owner_private_key,
+        owner_account_id,
+        vault_id,
+        vault_config_account_id,
+        vault_holding_account_id,
+    ) = state_deposited_with_mock_clock(
+        owner_balance_start,
+        deposit_amount,
+        mock_clock_account_id,
+        t0,
+    );
+
+    let stream_id = StreamId::MIN;
+    let stream_pda = derive_stream_pda(program_id, vault_config_account_id, stream_id);
+
+    let stream_accounts = [
+        vault_config_account_id,
+        vault_holding_account_id,
+        stream_pda,
+        owner_account_id,
+        mock_clock_account_id,
+    ];
+    transition_ok(
+        &mut state,
+        &signed_create_stream(
+            program_id,
+            vault_id,
+            stream_id,
+            provider_account_id,
+            rate,
+            allocation,
+            &stream_accounts,
+            Nonce(2),
+            &owner_private_key,
+        ),
+        3 as BlockId,
+        "create_stream failed",
+    );
+
+    force_mock_timestamp_account(&mut state, mock_clock_account_id, MockTimestamp::new(t1));
+
+    transition_ok(
+        &mut state,
+        &signed_sync_stream(
+            program_id,
+            vault_id,
+            stream_id,
+            &stream_accounts,
+            Nonce(3),
+            &owner_private_key,
+        ),
+        4 as BlockId,
+        "sync_stream failed",
+    );
+
+    let close_accounts: CloseStreamIxAccounts = [
+        vault_config_account_id,
+        vault_holding_account_id,
+        stream_pda,
+        owner_account_id,
+        alt_signer_account_id,
+        mock_clock_account_id,
+    ];
+
+    let r = state.transition_from_public_transaction(
+        &signed_close_stream(
+            program_id,
+            vault_id,
+            stream_id,
+            &close_accounts,
+            Nonce(0),
+            &alt_signer_private_key,
+        ),
+        5 as BlockId,
+    );
+    assert_execution_failed_with_code(r, ERR_CLOSE_UNAUTHORIZED);
 }
 
 #[test]
